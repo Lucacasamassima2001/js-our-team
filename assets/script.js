4a1db5adc9a29d884a8340ad9af0e9f766bb50aa
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-04-28 17:42:18 +0200

    ex done

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,63 @@
 // Consigli del giorno:
 // Ragioniamo come sempre a step.
 // Prima la logica in italiano e poi traduciamo in codice.
+
+
+// SELEZIONO GLI ELEMENTI
+
+const eleImg = document.querySelector(".member-img")
+const eleName = document.querySelector(".name")
+const eleJob = document.querySelector(".job")
+
+// CREO ARRAY CON I DATI
+
+
+const members = [
+    {
+        name: "Wayne Barnett",
+        job: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        job: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        job: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        job: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        job: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        job: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+console.log(members)
+
+let eleContainer = document.querySelector(".container");
+
+// CICLO PER CREAZIONE CARDS NEL DOM
+
+for(let i = 0; i < members.length; i++){
+    let member = members[i]
+    // console.log(member)
+    eleContainer.innerHTML += 
+    `<div class="card">
+    <img src="assets/imgs/${member.img}">
+    <div class="name">${member.name}</div>
+    <div class="job">${member.job}</div>
+    </div>`
+}
